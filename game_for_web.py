1e0cfff1c54ae2fa27b9830a1e38a599d9ce1ec9
quinncerino <quinncerino@gmail.com>
2025-07-16 01:06:42 -0400

    created web app and began implementing features

---
+++ game_for_web.py
@@ -0,0 +1,97 @@
+import requests
+from bs4 import BeautifulSoup
+from random import choice
+from pyfiglet import figlet_format
+from termcolor import colored
+from random import randint
+import sqlite3
+
+BASE_URL = "http://quotes.toscrape.com"
+
+def get_color():
+    colors = ("red", "green", "yellow", "blue", "magenta", "cyan", "blueviolet", "cadetblue", "coral", "cornflowerblue", "darkcyan", "darkgreen", "darkmagenta", "darkorchid", "darkslateblue", "darkseagreen", "deeppink", "darkturquoise", "deepskyblue", "dodgerblue", "hotpink", "indianred", "indigo", "lightcoral", "lightpink", "lightseagreen", "lightgreen", "mediumorchid", "mediumpurple", "mediumvioletred", "mediumspringgreen", "mediumturquoise", "orchid", "palevioletred", "steelblue")
+
+    choice = randint(0, len(colors)-1)
+    clr = colors[choice]
+
+    return clr
+
+
+def get_quotes_list(filename):
+    connection = sqlite3.connect(filename)
+    c = connection.cursor()
+    c.execute("SELECT * FROM quotes")
+    quotes_list = c.fetchall()
+    connection.commit()
+    connection.close()
+    return quotes_list
+
+
+def replace_info(string, sections):
+    if len(sections) == 0:
+        return string
+    else:
+        return replace_info(string.replace(sections[-1], "_______"), sections[:-1])
+
+
+def print_hint(quote, remaining_guesses, name):
+    res = requests.get(f"{BASE_URL}{quote[2]}")
+    soup = BeautifulSoup(res.text, "html.parser")
+    if remaining_guesses == 4:
+        return f"Sorry, that's incorrect! Here's a hint:\nHint #1: This author was born on {soup.find(class_='author-born-date').get_text()} {soup.find(class_='author-born-location').get_text()}"
+    elif remaining_guesses == 3:
+        return f"Sorry, that's incorrect! Here's a hint:\nHint #2: This author's first name starts with the letter '{name[0]}'"
+    elif remaining_guesses == 2:
+        space = name.rfind(' ')
+        return f"Sorry, that's incorrect! Here's a hint:\nHint #3: This author's last name starts with the letter '{name[space + 1]}'"
+    elif remaining_guesses == 1:
+        info = soup.find(class_='author-description').get_text()
+        sections = name.split(' ')
+        return f"Sorry, that's incorrect! Here's a hint:\nHint #4: Here is more info about this author.\n{replace_info(info, sections)}"
+    elif remaining_guesses == 0:
+        return f"Sorry, that's incorrect!\nYou ran out of guesses. The correct answer was: {name}"
+    else:
+        print(remaining_guesses)
+        return "error"
+
+
+
+def make_guess(quote, remaining_guesses, guess):
+    name = quote[1]
+    if guess.lower() == name.lower():
+        return "\nThat is correct! Congratulations, you've correctly guessed the author of the quote."
+
+    if remaining_guesses >= 0:
+        return print_hint(quote, remaining_guesses, name)
+    else:
+        return "Error"
+
+
+
+# def start_game(quotes):
+#     quote = choice(quotes)
+#     remaining_guesses = 5
+#     #get_color()
+#     print("Here's a quote: ")
+#     print(quote[0])
+#     #print(quote['author'])
+#     guess = ''
+#     name = quote[1]
+
+    # again = ''
+    # while again.lower() not in ('y','yes','n','no'):
+    #     again = input("Would you like to play again (Y/N)? ")
+    # if again.lower() in ('y','yes'):
+    #     print("\n")
+    #     return start_game(quotes)
+    # elif again.lower() in ('n', 'no'):
+    #     print("Okay, goodbye!")
+
+
+# def result_message(correct, remaining):
+#     if correct == True:
+#         return "\nThat is correct! Congratulations, you've correctly guessed the author of the quote."
+
+
+#quotes = get_quotes_list("quotes.db")
+#start_game(quotes)
--- web.py
+++ web.py
@@ -1,1 +1,90 @@
-import streamlit
+import streamlit as st
+import game_for_web
+from random import choice
+from time import sleep
+
+
+
+def decrement_guesses():
+    if st.session_state.remaining_guesses >= 0:
+        st.session_state.remaining_guesses -= 1
+    print(st.session_state.remaining_guesses)
+    check_win_lose()
+    if st.session_state.game_over == True:
+        st.session_state.guess = ""
+
+
+def new_round():
+    st.session_state["quote"] = choice(quotes)
+    st.session_state["remaining_guesses"] = 5
+    st.session_state["game_over"] = False
+
+
+def check_win_lose():
+    if st.session_state["guess"].lower() == st.session_state.quote[1].lower():
+        st.session_state.game_over = True
+        new_round()
+    elif st.session_state.remaining_guesses == 0:
+        st.session_state.game_over = True
+        new_round()
+
+
+st.set_page_config(layout = 'wide')
+
+
+st.markdown(
+    f"""<h1 style='
+        color: {game_for_web.get_color()}; 
+        font-family: "Optima";
+        font-size: 100px; 
+        text-align: center;
+        '>WORD FOR WORD</h1>""", 
+        unsafe_allow_html=True)
+ 
+
+col1, middle, col2 = st.columns([1.5, 0.1, 1.5])
+
+with col1:
+    st.image('images/whosaidthat.jpg')
+
+with col2:
+    st.title('"Who said that?!"')
+    st.info("""\
+            This is an interactive quote-guessing simulation. When given a quote, can you guess who said it, word for word?
+            
+            Each round, you will have 5 attempts to guess the person's name correctly.
+
+            After each incorrect guess, you will receive a helpful hint!
+
+            If you run out of guesses, you lose and the answer will be provided. Your score will be reset to 0 points.
+
+            If you can achieve 10 points by consecutively guessing ten correctly, then you win the game!""")
+
+
+
+st.header("Here's a quote:")
+
+quotes = game_for_web.get_quotes_list("quotes.db")
+
+if "quote" not in st.session_state:
+    st.session_state.quote = choice(quotes)
+    
+if "remaining_guesses" not in st.session_state:
+    st.session_state.remaining_guesses = 5
+
+if "game_over" not in st.session_state:
+    st.session_state.game_over = False
+
+quote = st.session_state.quote
+st.write(quote[0])
+
+
+guess_made = st.text_input(label="Enter your guess:", key="guess", on_change=decrement_guesses)
+
+
+
+if st.session_state.get("guess"):
+    st.info(game_for_web.make_guess(st.session_state.quote, st.session_state.remaining_guesses, st.session_state["guess"]))
+
+
+
